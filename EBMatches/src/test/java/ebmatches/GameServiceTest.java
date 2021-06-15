2319efd2fa0ec3cfd5b9c2e7afd35f026f48cf19
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-15 16:38:14 +0200

    Modify testGetLargestGoalDiffMatch() test method

--- EBMatches/src/test/java/ebmatches/GameServiceTest.java
+++ EBMatches/src/test/java/ebmatches/GameServiceTest.java
@@ -6,7 +6,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GameServiceTest {
@@ -19,7 +23,9 @@ class GameServiceTest {
 
     @Test
     void testGetLargestGoalDiffMatch() {
-
+        when(repository.getLargestGoalDiffMatch())
+                .thenReturn(Optional.of(new Game("Hungary", "Portugal", 4, 2)));
+        assertThat(service.getLargestGoalDiffMatch().get().getWinner()).isEqualTo("Hungary");
     }
 
 }
