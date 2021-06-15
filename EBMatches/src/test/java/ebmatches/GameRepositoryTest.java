5cf2319b813fc1bc5ff0b7618f6d97836def04b4
jfulop99 <66125323+jfulop99@users.noreply.github.com>
2021-06-15 16:51:35 +0200

    GameRepositoryTest addGameTest, addGamesFromFileTest methods

--- EBMatches/src/test/java/ebmatches/GameRepositoryTest.java
+++ EBMatches/src/test/java/ebmatches/GameRepositoryTest.java
@@ -1,7 +1,11 @@
 package ebmatches;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,11 +14,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameRepositoryTest {
 
-    GameRepository repository = new GameRepository(List.of(
+    GameRepository repository;
+
+    @BeforeEach
+    void setUp() {
+            repository = new GameRepository(List.of(
             new Game("Hungary", "Portugal", 3, 1),
             new Game("Hungary", "Italy", 6, 0),
             new Game("Italy", "Portugal", 2, 1)
     ));
+    }
 
     @Test
     void testGetLargestGoalDiffMatch() {
@@ -22,4 +31,19 @@ class GameRepositoryTest {
         assertEquals("Hungary", largestGoalDiff.get().getFirstCountry());
     }
 
+
+    @Test
+    void addGameTest() {
+        repository.addGame(new Game("Hungary", "Germany", 4, 1));
+        assertEquals(4, repository.getGames().size());
+        assertEquals("Germany", repository.getGames().get(3).getSecondCountry());
+    }
+
+    @Test
+    void addGamesFromFile() throws URISyntaxException {
+        Path file = Path.of("results.csv");
+        repository.addGamesFromFile(file);
+
+        assertEquals(18, repository.getGames().size());
+    }
 }
