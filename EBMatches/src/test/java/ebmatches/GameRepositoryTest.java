7ae03c401f9992497c6321d8dbd5f5e2becf5e95
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-15 16:22:28 +0200

    Change pom.xml, GameRepositoryTest.java

--- EBMatches/src/test/java/ebmatches/GameRepositoryTest.java
+++ EBMatches/src/test/java/ebmatches/GameRepositoryTest.java
@@ -1,7 +1,22 @@
 package ebmatches;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameRepositoryTest {
+
+    GameRepository repository = new GameRepository(List.of(
+            new Game("Hungary", "Portugal", 3, 1),
+            new Game("Hungary", "Italy", 6, 0),
+            new Game("Italy", "Portugal", 2, 1)
+    ));
+
+    @Test
+    void testGetLargestGoalDiffMatch() {
+
+    }
 
 }
